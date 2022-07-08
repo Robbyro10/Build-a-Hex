cb7d2e870f1b97b720cea216ac16da058252e260
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2022-07-07 22:07:12 -0400

    Update main.ts

--- Build a Hex/main.ts
+++ Build a Hex/main.ts
@@ -33,27 +33,24 @@ function setAdyacentes(tablero: Casilla[]): void{
 let tablero = crearCasillas(54);
 
 let ficha0 = new Ficha("rojo");
-insertar(ficha0, tablero[0]);
 let ficha1 = new Ficha("rojo");
-insertar(ficha1, tablero[1]);
 let ficha2 = new Ficha("rojo");
 let ficha3 = new Ficha("rojo");
 let ficha4 = new Ficha("rojo");
 let ficha5 = new Ficha("rojo");
-let ficha6 = new Ficha("rojo");
+
+//6 Casillas con su respectiva ficha y id
 let casilla0 = new Casilla(0, ficha0);
 let casilla1 = new Casilla(1, ficha1);
 let casilla2 = new Casilla(2, ficha2);
 let casilla3 = new Casilla(3, ficha3);
 let casilla4 = new Casilla(4, ficha4);
 let casilla5 = new Casilla(5, ficha5);
-let casilla6 = new Casilla(6, ficha6);
   
 casilla0.add(casilla1);
 casilla0.add(casilla2);
 casilla0.add(casilla3);
 casilla0.add(casilla4);
 casilla0.add(casilla5);
-casilla0.add(casilla6);
 console.log(casilla0.detectHex());
 console.log(casilla0.vecinos.length);
